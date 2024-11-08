0be8bf7d7f101ad1be4953b25f69c80afd515235
scaryjoshie <scaryjoshie@gmail.com>
2024-11-08 15:30:05 -0600

    testing some vals

---
+++ arduino-side/main/main.ino
@@ -0,0 +1,47 @@
+#include <Servo.h>
+int val;
+int dal;
+int kal;
+int val_init;
+int dal_init;
+int kal_init;
+
+// Midi Constants
+const int MKK = 36;
+const int MD1 = 37;
+const int MD2 = 38;
+const int MD3 = 39;
+const int MD4 = 40;
+const int MD5 = 41;
+const int MD6 = 42;
+
+int KK1 = 4;
+int KK2 = 7;
+Servo D1;
+Servo D2;
+Servo D3;
+Servo D4;
+Servo D5;
+Servo D6;
+
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(KK1, OUTPUT);
+  pinMode(KK2, OUTPUT);
+  D1.attach(3);
+  D2.attach(5);
+  D3.attach(6);
+  D4.attach(9);
+  D5.attach(10);
+  D6.attach(11);
+}
+
+void loop() {
+  D1.write(90);
+  delay(2000);
+  D1.write(80);
+  delay(2000);
+  D1.write(100);
+  delay(2000);
+}
