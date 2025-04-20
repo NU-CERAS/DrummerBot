0e3f9b11c6b09c6a967a90aab00eccd24f0702cd
Keigo Healy <afkhealy@gmail.com>
2025-04-20 11:45:22 -0500

    set up basic command structure

--- arduino-side/Tests/leo_spi_master_test/leo_spi_master_test.ino
+++ arduino-side/Tests/leo_spi_master_test/leo_spi_master_test.ino
@@ -1,21 +1,53 @@
 #include <SPI.h>
 
-volatile byte receivedValue = 0;
+#define START_MESSAGE 254;
+#define END_MESSAGE   255;
+
+volatile uint8_t command[5];      // to store incoming packet
+volatile uint8_t index = 0;      // current write index
+volatile bool messageReady = false;
 
 void setup() {
   Serial.begin(9600);
   pinMode(MISO, OUTPUT);       // Required for SPI slave
   pinMode(53, INPUT);          // SS must be input or low
+
   SPCR |= _BV(SPE);            // Enable SPI in slave mode
   SPI.attachInterrupt();       // Enable SPI interrupt
 }
 
+// ISR for SPI data receive
 ISR(SPI_STC_vect) {
-  receivedValue = SPDR;        // Store incoming byte
+  uint8_t data = SPDR;
+
+  // return if the message is first but not START_MESSAGE 
+  if (index == 0 && data != START_MESSAGE) return;
+
+  command[index] = data;
+  index++;
+
+  if (index == 5 && command[4] == END_MESSAGE) {
+    messageReady = true;
+    index = 0; // reset for next message
+  } else if (index >= 5) {
+    // out of sync or some other problem, reset
+    index = 0;
+  }
 }
 
 void loop() {
-  Serial.print("Received: ");
-  Serial.println(receivedValue);
-  delay(500);
+  if (messageReady) {
+    messageReady = false; //reset for next message
+
+    uint8_t device_type = command[1];
+    uint8_t device_pin  = command[2];
+    uint8_t position    = command[3];
+
+    Serial.println("Command Received:");
+    Serial.print("  Device Type: "); Serial.println(device_type);
+    Serial.print("  Device Pin:  "); Serial.println(device_pin);
+    Serial.print("  Position:    "); Serial.println(position);
+
+    //now do stuf with servo and pin 
+  }
 }
--- arduino-side/Tests/mega_spi_slave_test/mega_spi_slave_test.ino
+++ arduino-side/Tests/mega_spi_slave_test/mega_spi_slave_test.ino
@@ -1,6 +1,7 @@
 #include <SPI.h>
 
-uint16_t info = 12345;
+#define START_MESSAGE 254;
+#define END_MESSAGE   255;
 
 void setup() {
   pinMode(10, OUTPUT);   // SS must be output
@@ -8,13 +9,21 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
+void moveServo(uint8_t device_type, uint8_t device_pin, uint8_t position) {
   SPI.beginTransaction(SPISettings(500000, MSBFIRST, SPI_MODE0));
-  digitalWrite(10, LOW);           // Select slave
-  SPI.transfer(info);                // Send value
-  digitalWrite(10, HIGH);          // Deselect
-  SPI.endTransaction();
+  digitalWrite(pin, LOW);
 
-  Serial.println("Sent: 42");
+  SPI.transfer(START_MESSAGE);          // start message
+  SPI.transfer(device_type);  // device type
+  SPI.transfer(device_pin);   // specify device pin
+  SPI.transfer(position);     // specify device pin
+  SPI.transfer(END_MESSAGE);     // specify device pin
+
+  digitalWrite(pin, HIGH);
+  SPI.endTransaction();
+}
+
+void loop() {
+  moveServo(0, 0, 120);
   delay(1000);
 }
