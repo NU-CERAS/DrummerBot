552ae9940cd281b8dae58a57974ecd40f6952d51
Juliane Guo <jessicaguo934@gmail.com>
2025-04-20 17:35:50 -0500

    in-progress code for receiving messages from leornardo

--- arduino-side/Tests/leo_spi_master_test/leo_spi_master_test.ino
+++ arduino-side/Tests/leo_spi_master_test/leo_spi_master_test.ino
@@ -1,6 +1,14 @@
 #include <SPI.h>
 
-volatile byte receivedValue = 0;
+volatile byte receivedValues[4] = {0}; // order: start msg, device type, device id, position
+volatile byte byteIndex = 0;
+volatile bool ifComplete = false;
+
+#define TYPE_SERVO
+#define TYPE_STEPPER
+#define TYPE_SOLENOID
+
+// this is mega slave
 
 void setup() {
   Serial.begin(9600);
@@ -12,10 +20,37 @@ void setup() {
 
 ISR(SPI_STC_vect) {
   receivedValue = SPDR;        // Store incoming byte
+
+  // this code currently receives one byte, still need to:
+    // if not start byte,
+    // store it somewhere - array?
+  if (byteIndex < 4) {
+    receivedBytes[byteIndex++] = received; // <- increment happens here
+    if (byteIndex == 4) {
+      ifComplete = true;
+    }
+  }
+
 }
 
 void loop() {
-  Serial.print("Received: ");
-  Serial.println(receivedValue);
+  // once reaches 4 messages, adjust servos/motors
+  if (receivedValues[1] == TYPE_SERVO) {
+    // adjust servo
+    // device id: receivedValues[2]
+    // position: receivedValues[3]
+  }
+  else if (receivedValues[1] == TYPE_STEPPER) {
+    // adjust stepper motor
+  }
+  else if (receivedValues[1] == TYPE_SOLENOID) {
+    // adjust solenoid
+  }
+
+  Serial.println("Adjusted position");
+
+  byteIndex = 0;
+  ifComplete = false;
+
   delay(500);
 }
